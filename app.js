f0688b02e35e1887b6c16ef8986ee37a5592bc85
Oleg Ostashov <oleg.ostashov@gmail.com>
2018-04-10 12:51:50 +0300

    fixed pool.connect

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const config = require("./config");
-// const User = require("./models/user");
-// const Specialist = require("./models/specialist");
 const pg = require("pg");
-// const connectionString = process.env.DATABASE_URL || config.PG_URL;
 const pg_connect = {
   user: config.user,
   host: config.host,
@@ -18,10 +15,10 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var pool = new pg.Pool(pg_connect);
+
 app.get("/", (req, res) => {
-  var pool = new pg.Pool(pg_connect);
   const results = [];
-  // pg.connect(connectionString, (err, client, done) => {
   pool.connect(function(err, client, done) {
     // Handle connection errors
     if (err) {
@@ -29,60 +26,24 @@ app.get("/", (req, res) => {
       done();
       return res.status(500).json({ success: false, data: err });
     } else {
-      // SQL Query > Select Data
-      client.query("SELECT * FROM users", (err, res) => {
-        console.log(err);
-        // res.rows.forEach(function(item) {
-        //   results.push(item);
-        // });
-      });
-      // client.end();
+      client
+        .query("SELECT * FROM users")
+        .then(result => {
+          result.rows.forEach(function(item) {
+            results.push(item);
+          });
+          res.render("index", { users: results });
+        })
+        .catch(err => console.log(err));
       done();
     }
-    console.log(results);
-    res.render("index", { users: results });
   });
-  pool.end();
 });
-
-// User.find({})
-//   .then(users => {
-//     res.render("index", { users: users });
-//   })
-//   .catch(err => {
-//     res.status(200).json({ err: err });
-//   });
-// Specialist.aggregate([
-//   {
-//     $lookup: {
-//       from: "User",
-//       localfield: "_id",
-//       foreignfield: "userID",
-//       as: "doctor"
-//     }
-//   }
-// ]);
-// .exec(function(err, doctor) {
-//   console.log(doctor);
-//   console.log(err);
-// });
-// });
 
 app.get("/register", (req, res) => res.render("register")); // res.send("Hello World! What\'s up!"))
 app.post("/register", (req, res) => {
   const { login, password, firstname, lastname } = req.body;
   console.log(req.body);
-
-  // User.create({
-  //   login: login,
-  //   password: password
-  // }).then(user =>
-  //   Specialist.create({
-  //     firstname: firstname,
-  //     lastname: lastname,
-  //     userID: user.id
-  //   })
-  // );
 
   res.redirect("/");
 });
