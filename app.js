5216e1c115ed4b46187a2d1c398f8f0880f1a0a3
Oleg Ostashov <oleg.ostashov@gmail.com>
2018-04-10 03:05:29 +0300

    not working queries

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const User = require("./models/user");
-const Specialist = require("./models/specialist");
+const config = require("./config");
+// const User = require("./models/user");
+// const Specialist = require("./models/specialist");
+const pg = require("pg");
+// const connectionString = process.env.DATABASE_URL || config.PG_URL;
+const pg_connect = {
+  user: config.user,
+  host: config.host,
+  database: config.database,
+  password: config.password,
+  port: config.port_pg
+};
 
 const app = express();
 
@@ -9,44 +19,70 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  User.find({})
-    .then(users => {
-      res.render("index", { users: users });
-    })
-    .catch(err => {
-      res.status(200).json({ err: err });
-    });
-  Specialist.aggregate([
-    {
-      $lookup: {
-        from: "User",
-        localfield: "_id",
-        foreignfield: "userID",
-        as: "doctor"
-      }
+  var pool = new pg.Pool(pg_connect);
+  const results = [];
+  // pg.connect(connectionString, (err, client, done) => {
+  pool.connect(function(err, client, done) {
+    // Handle connection errors
+    if (err) {
+      console.log(err);
+      done();
+      return res.status(500).json({ success: false, data: err });
+    } else {
+      // SQL Query > Select Data
+      client.query("SELECT * FROM users", (err, res) => {
+        console.log(err);
+        // res.rows.forEach(function(item) {
+        //   results.push(item);
+        // });
+      });
+      // client.end();
+      done();
     }
-  ]);
-  // .exec(function(err, doctor) {
-  //   console.log(doctor);
-  //   console.log(err);
-  // });
+    console.log(results);
+    res.render("index", { users: results });
+  });
+  pool.end();
 });
+
+// User.find({})
+//   .then(users => {
+//     res.render("index", { users: users });
+//   })
+//   .catch(err => {
+//     res.status(200).json({ err: err });
+//   });
+// Specialist.aggregate([
+//   {
+//     $lookup: {
+//       from: "User",
+//       localfield: "_id",
+//       foreignfield: "userID",
+//       as: "doctor"
+//     }
+//   }
+// ]);
+// .exec(function(err, doctor) {
+//   console.log(doctor);
+//   console.log(err);
+// });
+// });
 
 app.get("/register", (req, res) => res.render("register")); // res.send("Hello World! What\'s up!"))
 app.post("/register", (req, res) => {
   const { login, password, firstname, lastname } = req.body;
   console.log(req.body);
 
-  User.create({
-    login: login,
-    password: password
-  }).then(user =>
-    Specialist.create({
-      firstname: firstname,
-      lastname: lastname,
-      userID: user.id
-    })
-  );
+  // User.create({
+  //   login: login,
+  //   password: password
+  // }).then(user =>
+  //   Specialist.create({
+  //     firstname: firstname,
+  //     lastname: lastname,
+  //     userID: user.id
+  //   })
+  // );
 
   res.redirect("/");
 });
--- database.js
+++ database.js
@@ -1,16 +1,87 @@
 const config = require("./config");
-const mongoose = require("mongoose");
+// const pg = require("pg");
+const { Pool, Client } = require("pg");
+const connectionString = process.env.DATABASE_URL || config.PG_URL;
 
-module.exports = () => {
-  return new Promise((resolve, reject) => {
-    mongoose.Promise = global.Promise;
-    mongoose.set("debug", true);
+const pool = new Pool({
+  connectionString: connectionString
+});
 
-    mongoose.connection
-      .on("error", error => reject(error))
-      .on("close", () => console.log("Database connection closed."))
-      .once("open", () => resolve(mongoose.connections[0]));
+// const client = new pg.Client(connectionString);
+const client = new Client({
+  connectionString: connectionString
+});
 
-    mongoose.connect(config.MONGO_URL);
-  });
-};
+client.connect();
+
+// client.query("select * from users;", (err, res) => {
+//   if (err) {
+//     console.log(err.stack);
+//   } else {
+//     console.log(res.rows);
+//   }
+//   client.end();
+// });
+
+////////////////////
+
+// (async () => {
+//   await client.connect();
+
+//   var query = await client.query(
+//     "CREATE TABLE IF NOT EXISTS users ( \
+// 	id serial, \
+// 	login text, \
+// 	email text, \
+// 	register_dttm timestamp, \
+// 	usertype_id numeric(9,2), \
+// 	deleted_flag numeric(9,2), \
+// 	PRIMARY KEY( id ) \
+//   ); \
+//   \
+//   CREATE TABLE IF NOT EXISTS specialists ( \
+// 	id serial, \
+// 	user_id numeric(9,2), \
+// 	organisation_id numeric(9,2), \
+// 	specialisation_id numeric(9,2), \
+// 	firstname text, \
+// 	lastname text, \
+// 	middlename text, \
+// 	birthdate date, \
+// 	deleted_flag numeric(9,2), \
+// 	PRIMARY KEY( id ) \
+//   );",
+//     (res, err) => {
+//       console.log(res);
+//     }
+//   );
+
+//   query = await client.query(
+//     "select * from users",
+//     // "insert into users(login, email) VALUES('qwe','rty');",
+//     (res, err) => {
+//       console.log(res);
+//     }
+//   );
+
+//   query = await client.query("select * from users;", (res, err) => {
+//     console.log(res);
+//     client.end();
+//   });
+// })();
+
+// const mongoose = require("mongoose");
+
+// module.exports = () => {
+//   return new Promise((resolve, reject) => {
+//     mongoose.Promise = global.Promise;
+//     mongoose.set("debug", true);
+
+//     mongoose.connection
+//       .on("error", error => reject(error))
+//       .on("close", () => console.log("Database connection closed."))
+//       .once("open", () => resolve(mongoose.connections[0]));
+
+//     mongoose.connect(config.MONGO_URL);
+//   });
+// };
