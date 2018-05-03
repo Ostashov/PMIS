f1a102055616bb2ed52491d4fafab4841db6a28e
Oleg Ostashov <oleg.ostashov@gmail.com>
2018-05-03 14:08:07 +0300

    TODO api/patient/new

--- config/express.js
+++ config/express.js
@@ -1,9 +1,10 @@
 // Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
-// var mongoose = require("mongoose");
+var mongoose = require("mongoose");
 var session = require("express-session");
-// var MongoStore = require("connect-mongo")(session);
+var MongoStore = require("connect-mongo")(session);
+var FileStore = require("session-file-store")(session);
 
 var config = require("./config");
 var routes = require("./../routes/routes");
@@ -21,19 +22,30 @@ var initApp = function() {
     })
   );
 
-  // mongoose.connect(config.MONGO_URL);
-
-  // sessions
-  app.use(
-    session({
-      secret: config.SESSION_SECRET,
-      resave: true,
-      saveUninitialized: false //,
-      // store: new MongoStore({
-      //   mongooseConnection: mongoose.connection
-      // })
-    })
-  );
+  if (process.env.NODE_ENV == "production") {
+    mongoose.connect(config.MONGO_URL);
+    // sessions
+    app.use(
+      session({
+        secret: config.SESSION_SECRET,
+        resave: true,
+        saveUninitialized: false,
+        store: new MongoStore({
+          mongooseConnection: mongoose.connection
+        })
+      })
+    );
+  } else {
+    // sessions
+    app.use(
+      session({
+        secret: config.SESSION_SECRET,
+        resave: true,
+        saveUninitialized: false,
+        store: new FileStore()
+      })
+    );
+  }
 
   app.set("view engine", "ejs");
   app.use(bodyParser.urlencoded({ extended: true }));
